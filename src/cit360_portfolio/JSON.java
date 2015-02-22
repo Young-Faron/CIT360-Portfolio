29ac0e75f29c0025c25dbc74e282bd310a47d05d
Young-Faron <flybob@q.com>
2015-02-21 19:19:10 -0700

    setup github

---
+++ src/cit360_portfolio/JSON.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit360_portfolio;
+
+/**
+ *
+ * @author Faron
+ */
+
+ //JavaScript Object Notation
+public class JSON {
+    // Setup Json Data Objects
+    
+    
+    
+}
---
+++ src/cit360_portfolio/JSON.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var user = {};
+var job = {};
+var skills = {};
+user ={name:"Faron", city:"Rigby", state:"ID", Email: "George@george.com", Phone: "208-123-4567",Age: 34};
+job = {role:"Administrator", Wage_type: "Salary", Wage: 75000};
+skills = {leadership:"Excellent", Computer:"Excellent", Teamplayer:"Good", Presenter: "Need Improvement"};
+
+// Optional 
+var total = {recruit:job, required:skills, applicant: user};
+
+
+
+
+
+
+
+
+
+
+
+
